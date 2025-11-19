692620310a6ec53a5658e71269e0fce1cf94864d
henrique.comp <henrique.comp@gmail.com>
2025-11-19 18:22:02 -0300

    Adicionando os endpoints legal_organization, organization e departments

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 # Importa o roteador de usuários que acabamos de criar
-from app.routers import user 
-from app.routers import login
+from app.routers import user, login, legal_organization, organization, department
 
 app = FastAPI(
     title="Executiva Cloud API",
@@ -28,6 +27,9 @@ app.add_middleware(
 # Todas as rotas em user.py serão acessíveis. Ex: /users/, /users/1, etc.
 app.include_router(user.router)
 app.include_router(login.router)
+app.include_router(legal_organization.router)
+app.include_router(organization.router)
+app.include_router(department.router)
 
 # Rota raiz simples para verificação
 @app.get("/", tags=["root"])
